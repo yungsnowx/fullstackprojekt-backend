75db639309470518e3f174205be7b4786a9aaed8
Eren Yaman <inf3904@hs-worms.de>
2023-05-09 18:12:16 +0200

    Warenkorb Tabelle hinzugefügt

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import {router as productRouter} from './routes/productRoutes.js'
 import {router as userRouter} from './routes/userRoutes.js'
 import {router as addressRouter} from './routes/addressRoutes.js'
 import {router as orderRouter} from './routes/orderRoutes.js'
+import {router as cartRouter} from './routes/cartRoutes.js'
+
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -15,6 +17,7 @@ app.use(productRouter)
 app.use(addressRouter)
 app.use(userRouter)
 app.use(orderRouter)
+app.use(cartRouter)
 
 app.listen(port, () => {
     console.log("Shop-Backend server listening on " + port)
