83fc7988050a4c258d7b4aa3e2bb07009e23ce17
tchou <tchoudjiaymar@gmail.com>
2023-05-27 22:18:20 +0200

    Pass

---
+++ src/Unittest/UserModel.test.js
@@ -0,0 +1,11 @@
+import {it,expect} from "@jest/globals"
+
+
+describe("that is a describtion", () =>{
+    it("test" ,() =>{
+        const value = 2
+        expect(2).toStrictEqual(2)
+    })
+})
+
+
