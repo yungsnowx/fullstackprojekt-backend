5c29ce87e7abcd67c0606c3b60d931113f415d2a
Marcel <marcelfrizler@mailbox.org>
2023-06-27 17:13:02 +0200

    Fix readUserFromRequest function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,21 +1,20 @@
 import {
-  getAll,
-  getByID,
-  removeByID,
-  save
+  getAll, getByID, removeByID, save
 } from "../models/userModel.js";
+import {readUserFromRequest} from "../utils/requestUtil.js";
 
 export class UserController {
-  
   async getAllUsersAction(request, response) {
     let users = await getAll();
     response.json(users);
   }
+
   async getUserByIdAction(request, response) {
     let id = request.params.id;
     let user = await getByID(id);
     response.json(user);
   }
+
   async addUserAction(request, response) {
     let jsonObject = readUserFromRequest(request);
     jsonObject.isAdmin = false;
@@ -34,20 +33,5 @@ export class UserController {
     let id = request.params.id;
     await removeByID(id);
     response.json();
-  }
-
-  readUserFromRequest(request) {
-    let body = request.body;
-    let userID = body.userID;
-    let vorname = body.vorname;
-    let nachname = body.nachname;
-    let isAdmin = body.isAdmin;
-
-    return {
-      userID: userID,
-      vorname: vorname,
-      nachname: nachname,
-      isAdmin: isAdmin,
-    };
   }
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,9 @@
 import Router from "express";
 import { UserController } from "../controllers/userController.js";
-import bodyParser from "body-parser";
 const userController = new UserController();
+
 const router = Router();
 const routeName = "users";
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: false }));
 
 router.get(`/${routeName}`, userController.getAllUsersAction);
 router.get(`/${routeName}/:id`, userController.getUserByIdAction);
